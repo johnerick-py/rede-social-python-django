dd93a46665cbd5afdc8aae092a46a251f6646446
johnerick-py <jhonerick147@outlook.com>
2022-10-27 00:10:05 -0300

    parei no meio de uploading posting

--- socialmedia/social_book/core/models.py
+++ socialmedia/social_book/core/models.py
@@ -1,6 +1,10 @@
+from distutils.command.upload import upload
+from email.policy import default
 from unittest.util import _MAX_LENGTH
 from django.db import models
 from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
 
 
 User = get_user_model()
@@ -15,3 +19,14 @@ class Profile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=100)
+    image = models.ImageField(upload_to='post_images')
+    caption = models.TextField()
+    created_at = models.DateTimeField(default=datetime.now())
+    numb_of_likes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.user
--- socialmedia/social_book/core/views.py
+++ socialmedia/social_book/core/views.py
@@ -9,7 +9,45 @@ from .models import Profile
 
 @login_required(login_url='signin')
 def index(request):
-    return render(request, 'index.html')
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
+    
+    return render(request, 'index.html', {'user_profile': user_profile})
+
+@login_required(login_url='signin')
+def upload(request):
+    return HttpResponse('<h1>Upload View</h1>')
+
+@login_required(login_url='signin')
+def settings(request):
+    
+    user_profile = Profile.objects.get(user=request.user)
+    
+    if request.method == 'POST':
+        
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+            
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+            
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+    
+    return render(request, 'setting.html', {'user_profile': user_profile})
 
 def signup(request):
 
@@ -31,12 +69,16 @@ def signup(request):
                 user.save()
                 
                 #login do usuario e redirecionamento para configuração do perfil
+                
+                user_login = User.objects.create_user(username=username, password=password)
+                auth.login(request, user_login)
+                
             
                 #criando o objeto de perfil para o novo usuario
                 user_model = User.objects.get(username=username)
                 new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
                 new_profile.save()
-            return redirect('signup')
+            return redirect('settings')
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
@@ -62,6 +104,7 @@ def signin(request):
 
         return render(request, 'signin.html')
         
+@login_required(login_url='signin')
 def logout(request):
     auth.logout(request)
     return redirect('signin')
